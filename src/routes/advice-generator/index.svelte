099bf98036180cf79ece64cd1f05bdfe36fb247a
Badu Blanc <itsbadu@hey.com>
2022-02-25 10:47:40 -0600

    update spacing

--- src/routes/advice-generator/index.svelte
+++ src/routes/advice-generator/index.svelte
@@ -25,7 +25,7 @@
 
 <main class="min-h-screen page-bg flex flex-col items-center justify-center">
 	<div
-		class="dark-gray-bg flex flex-col space-y-6 max-w-xl items-center rounded-2xl px-10 md:px-12 pt-12 pb-20 manrope mx-6"
+		class="dark-gray-bg flex flex-col space-y-6 max-w-xl items-center rounded-2xl px-10 md:px-12 pt-12 pb-16 manrope mx-6"
 	>
 		{#if loading}
 			<img src="/assets/loading-2.gif" alt="" class="h-32 w-32 mb-3" />
@@ -34,10 +34,10 @@
 			<p class="quote-text text-center px-2 pb-2 leading-snug">"{data.advice}"</p>
 		{/if}
 
-		<img src="/assets/ag-divider-desktop.svg" alt="" class="" />
+		<img src="/assets/ag-divider-desktop.svg" alt="" class="pt-2" />
 	</div>
 
-	<button on:click={getAdvice} class="button-bg p-4 rounded-full -mt-7" aria-label="Refresh advice">
+	<button on:click={getAdvice} class="button-bg p-5 rounded-full -mt-8" aria-label="Refresh advice">
 		<img src="/assets/ag-dice.svg" alt="" class="" />
 	</button>
 </main>
